9c28b324a2ae1e839b5b7a944bc0f320cd9f51df
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2021-02-26 13:37:51 -0800

    Updated Page.class with Logo
    Added logo to navigation bar, removing the placeholder for resting purposes.

--- inc/utilities/Page.class.php
+++ inc/utilities/Page.class.php
@@ -21,7 +21,7 @@ static function header(){ ?>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Ecommerce Website</a>
+                <a class="navbar-brand" href="index.php"><img src="imgs/ck_logo.jpg" alt="Cozy Kaffe Logo" srcset="" width="45px"></a>
 
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                     <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
