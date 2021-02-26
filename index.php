bfcae654a0c03a0edcc0ee3bce57c56d53a05c83
LindseyGunderson <65924727+LindseyGunderson@users.noreply.github.com>
2021-02-26 13:06:23 -0800

    Adjusting the index page
    Adjusting the index page and add comments to explain each step.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// require the database configurations
+// require database configurations
 require_once('inc/config.inc.php');
 
 // entity classes
@@ -11,21 +11,22 @@ require_once('inc/entities/ProductMapper.class.php');
 include_once('inc/utilities/PDOAgent.class.php');
 require_once('inc/utilities/Page.class.php');
 
-    // title for the index page
-    PageIndex::$title = "";
+    // Set the title of the homepage
+    PageIndex::$title = "Cozy Kaffe Homepage";
 
-    // header for the index page
+    // display the header
     PageIndex::header();
 
-    // Initialize the connection to the database to access the product list
+    // initialize the connection to the database
     ProductMapper::initialize("Products");
 
-    // query the products from the database
+    // Display "recently" added products
+    // This will show 3 random products each time the page is loaded from the database
     $products = ProductMapper::getRecentlyAddedProducts();
 
-    // show all the products in a list from the query
-    // This will show a limited amount of products
-    PageIndex::showProducts($products);
+    // Display all the products and heading text for the page
+    PageIndex::showProducts($products, "Recently Added Products");
+
 
     // include the footer
     PageIndex::footer();
